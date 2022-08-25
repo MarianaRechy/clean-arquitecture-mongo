6b2841fada3f5d1fd77c973630bbc75eeb5c19da
rechy <rechy.mr@gmail.com>
2022-08-24 21:41:04 -0500

    update240822

--- src/routes/generation.router.js
+++ src/routes/generation.router.js
@@ -8,22 +8,24 @@ const {  create } = require("../usecases/generation.usecases")
 const router = express.Router()
 
 router.post("/", async (request,response) => {
+
     try{
-        const { query } = request
-        const gen = await create(query)
+        const { body } = request
+        const koder = await create(body)
+        response.status(201)
         response.json({
             success: true,
             data:({
-                gen
+                koder,
             })
         })
-    }catch(error) {
+    }catch(error){
         response.status(400)
         response.json({
-            success: false,
-            message: error.message
+          success: false,
+          message: error.message
         })
-    }
+      }
 })
 
 module.exports = router
